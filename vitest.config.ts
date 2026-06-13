e16bc8bedab6aa5e9b14078bc6ab5f57b9a1abe2
Uditi Jangid <uditijangid20@gmail.com>
2026-06-13 17:35:05 +0530

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
         }
     },
     test: {
-        environment: 'node',
+        environment: 'nodes',
         clearMocks: true,
         restoreMocks: true,
         mockReset: true,
